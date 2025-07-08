b7f42388c840c4706b1869c2c66f8191f68a9bc7
Your Name <you@example.com>
2025-07-08 10:50:13 -0400

    Agregando Login con google

--- djangoTemplate/{{ project_name }}/settings.py
+++ djangoTemplate/{{ project_name }}/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import pymysql
+pymysql.install_as_MySQLdb()
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +44,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #Aqui esta todo lo relacionado a la libreria allauth
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
+    #Este es la red social que vamos a utilizar (Esto puede variar)
+    'allauth.socialaccount.providers.google',
 ]
 
 
@@ -54,6 +64,10 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    
+    #Necesario para poder utilizar django-allauth
+    'allauth.account.middleware.AccountMiddleware',
+    
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -63,6 +77,32 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000'
 
 ]
+
+#Importante para usar django-allauth
+AUTHENTICATION_BACKENDS= (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend"
+)
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION ="none"
+SOCIALACCOUNT_AUTO_SIGNUP = True
+#Importante para elminar la pagina intermedio de django
+SOCIALACCOUNT_LOGIN_ON_GET = True
+
+#Aqui para poder agregar la red social deseada
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,
+    }
+}
 
 
 
